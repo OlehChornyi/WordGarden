6de7ec982249c32c5c65ab432756b6bd50e530f6
oleh <phd.chornyi@gmail.com>
2025-01-15 11:16:10 +0200

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Oleh on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
